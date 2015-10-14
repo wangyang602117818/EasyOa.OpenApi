201df526d34e44dd0a51c4839da66eb80d980f7b
wangyang <wang.yang@etaostars.com>
2015-10-14 09:50:51 +0800

    sdf

--- EasyOa.OpenApi/Controllers/HomeController.cs
+++ EasyOa.OpenApi/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EasyOa.Common;
 
 namespace EasyOa.OpenApi.Controllers
 {
@@ -10,6 +11,7 @@ namespace EasyOa.OpenApi.Controllers
     {
         public ActionResult Index()
         {
+            
             return View();
         }
     }
