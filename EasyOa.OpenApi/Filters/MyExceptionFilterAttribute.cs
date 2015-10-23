89f5cf3431d3706f20a2020e1d5df7a57024206a
wangyang <wang.yang@etaostars.com>
2015-10-23 18:56:46 +0800

    sdf

--- EasyOa.OpenApi/Filters/MyExceptionFilterAttribute.cs
+++ EasyOa.OpenApi/Filters/MyExceptionFilterAttribute.cs
@@ -11,7 +11,7 @@ using EasyOa.OpenApi.Exception;
 namespace EasyOa.OpenApi.Filters
 {
     /// <summary>
-    /// 自定义全局webapi异常处理
+    /// 自定义全局webapi异常处理,以后的代码里面就不需要写try catch捕获异常了
     /// </summary>
     public class MyExceptionFilterAttribute : ExceptionFilterAttribute
     {
