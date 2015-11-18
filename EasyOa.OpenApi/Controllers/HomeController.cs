d79bd575fa3f61cab7d461c0ec797bf3dd0afaae
wangyang <wang.yang@etaostars.com>
2015-11-18 15:28:44 +0800

    dsf

--- EasyOa.OpenApi/Controllers/HomeController.cs
+++ EasyOa.OpenApi/Controllers/HomeController.cs
@@ -15,47 +15,13 @@ namespace EasyOa.OpenApi.Controllers
     {
         public ActionResult Index()
         {
-            In();
-
-            //LogDequeue<Person>.DoDequeue(ConsumeMessage);
-            //LogHelper.MailLog(new System.Exception("sdsfds"));
+            
 
 
             return View();
         }
-        public bool ConsumeMessage(Person person)
-        {
-            string str = JsonSerializerHelper.Serialize(person);
-            LogHelper.InfoLog(str);
-            if (person.Age == 12)
-            {
-
-            }
-            return true;
-        }
-        public void In()
-        {
-            List<Person> list = new List<Person>();
-            for (int i = 0; i < 10; i++)
-            {
-                Person p = new Person()
-               {
-                   Age = i,
-                   Name = "张三" + i
-               };
-                list.Add(p);
-            }
-
-            //Person1 p1 = new Person1()
-            //    {
-            //        A = 10,
-            //        N = "asv"
-            //    };
-            foreach (Person person in list)
-            {
-                LogEnqueue.Enqueue(person);
-            }
-        }
+        
+       
     }
 
 }
