efd6671c25704711f6a3cfc435467a0088f3cfa0
wangyang_thinkpad <602117818@qq.com>
2015-12-03 21:54:12 +0800

    sdf

--- EasyOa.OpenApi/Controllers/AlipayController.cs
+++ EasyOa.OpenApi/Controllers/AlipayController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using EasyOa.Common.Alipay.DirectPay;
+using EasyOa.Common.Properties;
 using EasyOa.OpenApi.Models;
 
 namespace EasyOa.OpenApi.Controllers
@@ -25,5 +27,21 @@ namespace EasyOa.OpenApi.Controllers
         {
             return person;
         }
+    }
+
+    public class Person
+    {
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(Resources))]
+        [StringLength(5, MinimumLength = 5, ErrorMessageResourceName = "StringLength", ErrorMessageResourceType = typeof(Resources))]
+        public string Name { get; set; }
+
+        //[Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(Resources))]
+        [Range(10, 100, ErrorMessageResourceName = "Range", ErrorMessageResourceType = typeof(Resources))]
+        public int? Age { get; set; }
+    }
+    public class Person1
+    {
+        public string N { get; set; }
+        public int A { get; set; }
     }
 }
